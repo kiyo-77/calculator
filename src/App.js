f278e4ebbf54a575aa3860d1911164a13085eeaf
Krupa Sri <srinivasankrupa7@gmail.com>
2023-10-14 20:42:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calculator from "./components/Calculator";
+import "./App.css";
+function App() {
+  return (
+    <><div className="back">
+      <Calculator />
+      </div>
+    
+    </>
+  );
+}
+
+export default App;
